3429e585f197d34cb7b38db7fb7b345879442524
Febby B. Simanjuntak <klocsimanjuntak@gmail.com>
2021-10-10 08:43:01 +0700

    [Febby] Header and Footer

---
+++ reactjs/src/footer/Footer.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import { Card, CardGroup } from 'react-bootstrap'
+
+
+function Footer() {
+  return (
+    <CardGroup className="fixed-bottom">
+      <Card>
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+            This is a wider card with supporting text below as a natural lead-in to
+            additional content. This content is a little bit longer.
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <small className="text-muted">Last updated 3 mins ago</small>
+        </Card.Footer>
+      </Card>
+      <Card>
+        <Card.Body>
+          <Card.Title>Sitemap</Card.Title>
+          <Card.Text>
+            This is a wider card with supporting text below as a natural lead-in to
+            additional content. This content is a little bit longer.
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <small className="text-muted">Last updated 3 mins ago</small>
+        </Card.Footer>
+      </Card>
+      <Card>
+        <Card.Body>
+          <Card.Title>Contact Us</Card.Title>
+          <Card.Text>
+            This card has supporting text below as a natural lead-in to additional
+            content.{' '}
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <small className="text-muted">Last updated 3 mins ago</small>
+        </Card.Footer>
+      </Card>
+    </CardGroup>
+  );
+}
+
+export default Footer;
